f2e4b9065d8049d531906fb317f7236a3f0c9d31
HeatherChloe <csy_cn@qq.com>
2016-05-19 16:05:39 +0800

    finished

---
+++ src/fibonacci_series.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var fiblist = [];
+//var fib_arr = [];
+	fiblist[0] = 0;
+	fiblist[1] = 1;
+
+function fibonacci_series(n) 
+{
+	//alert("in function");
+	//输入n 输出n个斐波那契数 fib_arr[0]...fib_arr[n]
+	if(n <=2)
+	{
+		return;
+    }
+
+	fibonacci_series(n-1);
+	
+	fiblist.push((fiblist[n-2]+fiblist[n-3]));
+
+}
+
+//n = 5;
+//fibonacci_series(n);
+//alert(fiblist);
+module.exports = fibonacci_series;
